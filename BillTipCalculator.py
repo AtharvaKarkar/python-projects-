722153bc7cb8b10ac2d11405df671413b44aa98f
AtharvaKarkar <93367238+AtharvaKarkar@users.noreply.github.com>
2023-01-14 11:23:46 +0530

    Add files via upload

---
+++ BillTipCalculator.py
@@ -0,0 +1,15 @@
+# this is a small project to understand data type,data manipulation in python
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
+people = int(input("How many people to split the bill?"))
+
+tip_as_percent = tip / 100
+total_tip_amount = bill * tip_as_percent
+total_bill = bill + total_tip_amount
+bill_per_person = total_bill / people
+final_amount = round(bill_per_person, 2)
+
+print(f"Each person should pay: ${final_amount}")
+# F-string are faster than the 2 most commonly used string formatting mechanisms
+# Python f-string is the newest Python syntax to do string formatting.
