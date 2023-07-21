7221f0a6f0dde32f92262f6b15fb8c5a35227f5b
AtharvaKarkar <93367238+AtharvaKarkar@users.noreply.github.com>
2023-07-21 23:09:18 +0530

    push

--- Robo_Speaker.py/main.py
+++ Robo_Speaker.py/main.py
@@ -2,7 +2,13 @@ import os
  
 if __name__ == '__main__':
     print("Welcome to ROBOSPEAKER 1")
-    msg = input("Enter a text which you want me to speak")
-    command = f"say {msg}"
-    os.system(command)
+    while True:
+        msg = input("Enter a text which you want me to speak")
+        if  msg =="q":
+            break
+        command = f"say {msg}"
+        os.system(command)
+        
+        
+   
     
